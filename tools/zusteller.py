e23b075abab2abd5d503483c20dff94a001f1143
Pilen <fiskomaten@gmail.com>
2014-05-07 11:08:45 +0200

    added size constraint and verbosity to zusteller

--- tools/zusteller.py
+++ tools/zusteller.py
@@ -1,43 +1,55 @@
 
 import socket
 
-ip = "127.0.0.1"
+host = "127.0.0.1"
 port = 1234
 message = "Hello, World!"
 
-def udp(ip, port, message):
+def udp(host, port, message, size=None, verbose=False):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    sock.sendto(bytes(message, "utf-8"), (ip, int(port)))
+    bytemessage = bytes(message, "utf-8")
+
+    if size is not None:
+        bytemessage = (bytemessage + bytes(max(0, size - len(bytemessage))))[:size]
+
+    if verbose:
+        print(bytemessage)
+
+    sock.sendto(bytemessage, (host, int(port)))
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("IP", help="IP of the host")
-    parser.add_argument("port", help="port of the host")
-    parser.add_argument("message", help="The content of the message to send")
+    parser.add_argument("host", help="IP or hostname of the host.")
+    parser.add_argument("port", help="port of the host.")
+    parser.add_argument("message", help="The content of the message to send.")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-u", "--UDP", help="Use the UDP protocol", action="store_const", const="udp", default="udp", dest="protocol")
-    group.add_argument("-t", "--TCP", help="Use the TCP protocol", action="store_const", const="tcp", dest="protocol")
+    group.add_argument("-u", "--UDP", help="Use the UDP protocol (default).", action="store_const", const="udp", default="udp", dest="protocol")
+    group.add_argument("-t", "--TCP", help="Use the TCP protocol.", action="store_const", const="tcp", dest="protocol")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-n", "--newlines", help="turn \\n into newlines", action="store_true", default=True)
-    group.add_argument("-N", "--no-newlines", help="do NOT turn \\n into newlines", action="store_false", dest="newlines")
+    group.add_argument("-n", "--newlines", help="turn \\n into newlines (default).", action="store_true", default=True)
+    group.add_argument("-N", "--no-newlines", help="do NOT turn \\n into newlines.", action="store_false", dest="newlines")
+    parser.add_argument("-s", "--size", help="Specify a fixed size for the data, truncating message if longer.", type=int)
+    parser.add_argument("-v", "--verbose", help="Increase verbosity.", action="store_true")
     args = parser.parse_args()
 
-    ip = args.IP
+    host = args.host
     port = args.port
     message = args.message
 
     protocol = args.protocol
     newlines = args.newlines
+    size = args.size
+    verbose = args.verbose
 
     if newlines:
         # Unescape escaped string by converting it to a bytestream and decoding it back to unicode
         message = bytes(message, "utf-8").decode("unicode-escape")
 
     if protocol == "udp":
-        udp(ip, port, message)
+        udp(host, port, message, size, verbose)
     elif protocol == "tcp":
         print("TCP protocol not yet supported")
