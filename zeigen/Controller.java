14adcbed87620bdf1aa7964c99a977184ee92eb2
Pilen <fiskomaten@gmail.com>
2014-05-12 00:03:25 +0200

    fixed some bugs

--- zeigen/Controller.java
+++ zeigen/Controller.java
@@ -84,7 +84,7 @@ public class Controller {
         if (this.sketchName.equals(sketch)) {
             this.kill();
         } else {
-            System.out.println("CAN'T KILL: " + message);
+            System.out.println("CAN'T KILL: " + sketch);
         }
     }
     private void kill() {
@@ -116,7 +116,7 @@ public class Controller {
 
         if (this.sketch != null) {
             this.sketch.zStart(sketchName);
-            this.sketchName = sketch;
+            this.sketchName = sketchName;
         } else {
             System.out.println("COULD NOT LOAD CLASS: " + sketchName);
         }
