14a5a174f080cbfd88e9572d80d0f3400075f075
Pilen <fiskomaten@gmail.com>
2014-05-11 14:44:19 +0200

    added a test sketch

---
+++ zeigen/TestSketch.java
@@ -0,0 +1,16 @@
+
+public class TestSketch extends ZSketch {
+    private int t;
+
+    public void zSetup() {
+        size(400, 400);
+        background(140, 140, 200);
+        background(random(255), random(255), random(255));
+        this.t = 0;
+    }
+
+    public void zDraw() {
+        ellipse(200 + (t % 4)*10,200,100,100);
+        this.t++;
+    }
+}
