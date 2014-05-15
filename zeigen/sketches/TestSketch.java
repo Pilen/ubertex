c3277e395ae828357f3cd630681f9aa63fd1020e
Pilen <fiskomaten@gmail.com>
2014-05-15 14:48:39 +0200

    now the test sketch use the default sizing

--- zeigen/sketches/TestSketch.java
+++ zeigen/sketches/TestSketch.java
@@ -3,7 +3,7 @@ public class TestSketch extends ZSketch {
     private int t;
 
     public void zSetup() {
-        size(400, 400);
+        // size(400, 400);
         background(140, 140, 200);
         // background(random(255), random(255), random(255));
         this.t = 0;
