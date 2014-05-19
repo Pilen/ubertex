1253a0fb365f225cac47d723b51ed3af83af5149
Pilen <fiskomaten@gmail.com>
2014-05-19 14:10:53 +0200

    Time now knows which TimeUnit it is using

--- zeigen/Time.java
+++ zeigen/Time.java
@@ -1,7 +1,10 @@
 
+import java.util.concurrent.TimeUnit;
+
 public class Time {
     private static long offset = 0;
 
+    public static final TimeUnit timeUnit = TimeUnit.MILLISECONDS;
     public static long now() {
         return System.currentTimeMillis() + offset;
     }
