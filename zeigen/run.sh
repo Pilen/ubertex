ca66c24109e78b20bb037acf64456c2fc497eba4
Pilen <fiskomaten@gmail.com>
2014-05-25 01:42:37 +0200

    fooling around with video path

--- zeigen/run.sh
+++ zeigen/run.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-CLASSPATH="/usr/share/processing/core/library/core.jar:.:.."
+CLASSPATH="/usr/share/processing/core/library/core.jar:.:..:./video.jar:/usr/share/processing/modes/java/libraries/video/library/gstreamer-java.jar:/usr/share/processing/modes/java/libraries/video/library/jna.jar"
+# ./video.jar:./gstreamer-java.jar:./jna.jar"
 
 echo "Compiling"
 javac -cp $CLASSPATH *.java
@@ -11,3 +12,4 @@ javac -cp "$CLASSPATH:./sketches/" sketches/*.java
 
 echo "Running"
 /usr/share/processing/java/bin/java -cp $CLASSPATH Zeigen "$@"
+# java -cp $CLASSPATH Zeigen "$@"
