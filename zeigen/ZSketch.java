86d25af185bca9574e3fbf0e6b2fe2a2a436c3f2
Pilen <fiskomaten@gmail.com>
2014-05-28 02:54:46 +0200

    updated ZSketch

--- zeigen/ZSketch.java
+++ zeigen/ZSketch.java
@@ -1,11 +1,52 @@
 
 public final class ZSketch {
 
-    public ZSketch() {
+    private Controller controller;
+
+    public ZSketch(Controller controller) {
         super();
+
+        this.controller = controller;
     }
 
     public void zStart() {
+        this.runSketch(new String[] {"--hide-stop"});
+    }
 
+    public void setup() {
+        controller.reset();
+        size(controller.width, controller.height);
+        background();
+        frame.setBackground(new java.awt.Color(0, 0, 0));
+        //The above does not currently work so we use a workaround, see https://github.com/processing/processing/issues/2071
+        ((javax.swing.JFrame) frame).getContentPane().setBackground(new java.awt.Color(0,0,0));
+
+    }
+
+    public void draw() {
+        try {
+            controller.step();
+            controller.module.draw();
+            if (controller.blank) {
+                background()
+            }
+        } catch (Exception e) {
+            System.out.println("======== ERROR ========\n" + "CURRENT MODULE THREW AN EXCEPTION\n" + e.toString());
+            e.printStackTrace();
+            System.out.println("=======================");
+            exit();
+        }
+    }
+
+    public File dataFile(String filename) {
+        File absolute = new File(filename);
+        if (absolute.isAbsolute()) {
+            return absolute;
+        }
+        return new File(controller.baseDir, filename);
+    }
+
+    public void background() {
+        background(controller.backgroundColor);
     }
 }
