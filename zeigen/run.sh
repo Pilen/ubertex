ec32ef83bb48f5e939a5d2e0a8d4df05410d8bdd
Pilen <fiskomaten@gmail.com>
2014-05-29 05:46:03 +0200

    updated run.sh

--- zeigen/run.sh
+++ zeigen/run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 set -e
-DIR=`dirname $0`
+
+DIR=$(dirname $(readlink -f $0))
 
 PROCESSING="/usr/share/processing"
 if [ ! -d "$PROCESSING" ]; then
-    echo "not found"
+    echo "$PROCESSING not found"
     PROCESSING="$DIR/processing-2.2.1"
+    echo "Trying $PROCESSING"
 fi
 
 CLASSPATH="$PROCESSING/core/library/core.jar:$DIR:$DIR/..:$DIR/default-modules/:./video.jar:$PROCESSING/modes/java/libraries/video/library/gstreamer-java.jar:$PROCESSING/modes/java/libraries/video/library/jna.jar"
@@ -14,9 +16,23 @@ CLASSPATH="$PROCESSING/core/library/core.jar:$DIR:$DIR/..:$DIR/default-modules/:
 
 echo "Compiling"
 javac -cp $CLASSPATH *.java
+
 echo "Compiling sketches"
 javac -cp "$CLASSPATH" default-modules/*.java
+javac -cp "$CLASSPATH" modules/*.java
 
-echo "Running"
-$PROCESSING/java/bin/java -cp $CLASSPATH Zeigen "$@"
-# java -cp $CLASSPATH Zeigen "$@"
+if [[ $# == 1 ]]; then
+    echo "Deploying modules"
+    # TODO: copy only .class files
+    echo "Transferring modules"
+    cp -r "$DIR/modules/." "$1/modules/"
+    sh trans.sh
+fi
+
+if [[ $# == 3 ]]; then
+    echo "Deploying modules"
+    cp -r "$DIR/modules/." "$3/modules/"
+    echo "Running"
+    $PROCESSING/java/bin/java -cp $CLASSPATH Zeigen "$@"
+    # java -cp $CLASSPATH Zeigen "$@"
+fi
