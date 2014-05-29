f2972c168c06a610b159098983568b9246da7b14
Pilen <fiskomaten@gmail.com>
2014-05-29 07:33:04 +0200

    generalised renderImage using the mode string

--- zeigen/ImageRenderer.java
+++ zeigen/ImageRenderer.java
@@ -3,13 +3,27 @@ import processing.core.PImage;
 
 public abstract class ImageRenderer extends Module {
 
-
     public void renderImage(PImage image, String mode) {
-        this.renderImage(image, mode, 0, 0, width, height);
-    }
+        float x, y, w, h;
 
-    public void renderImage(PImage image, String mode, float offsetX, float offsetY, float width, float height) {
-        float x = 0, y = 0, w = 0, h =0;
+        float offsetX = 0;
+        float offsetY = 0;
+        float width = this.width;
+        float height = this.height;
+
+        String[] parts = mode.split("/", 2);
+        if (parts.length == 2) {
+            mode = parts[0];
+            parts = parts[1].split(",")
+            try {
+                offsetX = Tools.parseFloat(parts[0]);
+                offsetY = Tools.parseFloat(parts[1]);
+                width = Tools.parseFloat(parts[2]);
+                height = Tools.parseFloat(parts[3]);
+            } catch (NumberFormatException e) {
+                println("Wrong mode specifier");
+            }
+        }
 
         switch(mode) {
         default: println("Unknown mode: " + mode);
