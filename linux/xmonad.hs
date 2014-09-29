fc4d9dae13dfa32652a0b59832086294bd965a3e
Pilen <fiskomaten@gmail.com>
2014-09-29 14:26:13 +0200

    added default xmonad.hs

---
+++ linux/xmonad.hs
@@ -0,0 +1,11 @@
+import XMonad
+  import XMonad.Layout.NoBorders
+
+  main = xmonad $ defaultConfig {
+    terminal = "urxvt",
+    borderWidth = 1,
+    normalBorderColor  = "#000000",
+    focusedBorderColor = "#000000",
+    modMask = mod4Mask, -- Set win as mod key
+    layoutHook = smartBorders $ layoutHook defaultConfig
+    }
