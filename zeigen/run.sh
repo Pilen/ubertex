df635d186c060c08c67c63555080aae89f750e7c
Pilen <fiskomaten@gmail.com>
2014-09-29 16:44:12 +0200

    updated run.sh so it will try to guess a display when none is set

--- zeigen/run.sh
+++ zeigen/run.sh
@@ -4,6 +4,12 @@ set -e
 
 DIR=$(dirname $(readlink -f $0))
 
+if [ -z $DISPLAY ]; then
+    echo "No DISPLAY set, assuming DISPLAY=:0"
+    DISPLAY=:0
+fi
+
+
 PROCESSING="/usr/share/processing"
 
 if [ ! -d "$PROCESSING" ]; then
@@ -24,7 +30,7 @@ if [ -d "$DIR/modules/" ]; then
     javac -cp "$CLASSPATH" modules/*.java
 fi
 
-if [[ $# == 1 ]]; then
+if [ $# == 1 ]; then
     echo "Deploying modules"
     # TODO: copy only .class files
     echo "Transferring modules"
@@ -32,7 +38,7 @@ if [[ $# == 1 ]]; then
     sh trans.sh
 fi
 
-if [[ $# == 3 ]]; then
+if [ $# == 3 ]; then
     if [ -d "$DIR/modules/." ]; then
         echo "Deploying modules"
         cp -u -r "$DIR/modules/." "$3/modules/"
