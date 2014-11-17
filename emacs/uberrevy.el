4ad40f358e5909f89500fbcb9511b7fdf2aaf70b
Pilen <fiskomaten@gmail.com>
2014-11-17 17:47:02 +0100

    added goto-match-paren

--- emacs/uberrevy.el
+++ emacs/uberrevy.el
@@ -269,6 +269,25 @@ Used like (revy-on-worker revy-worker-brok 'revy-play-sound \"sound.mp3\")"
     (apply function args)))
 
 
+(defun goto-match-paren ()
+  "Go to the matching parenthesis if on parenthesis. Else go to the
+   opening parenthesis one level up."
+  (interactive)
+  (cond ((looking-at "\\s\(") (forward-list 1))
+        (t
+         (backward-char 1)
+         (cond ((looking-at "\\s\)")
+                (forward-char 1) (backward-list 1))
+               (t
+                (while (not (looking-at "\\s("))
+                  (backward-char 1)
+                  (cond ((looking-at "\\s\)")
+                         (message "->> )")
+                         (forward-char 1)
+                         (backward-list 1)
+                         (backward-char 1)))
+                  ))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;π Includes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
