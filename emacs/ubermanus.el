9e4003c3fc0d1ca63845bf9ab8a732939e9eaf53
Pilen <fiskomaten@gmail.com>
2014-11-25 20:38:11 +0100

    changed ubermanus to use f2 instead of f6

--- emacs/ubermanus.el
+++ emacs/ubermanus.el
@@ -12,9 +12,9 @@ is therefore not required to be enabled for editing a manus, as
 the functions can be called on their own."
   :lighter "manus"
   :keymap (let ((revy-manus-mode-map (make-sparse-keymap)))
-            (define-key revy-manus-mode-map (kbd "<f6>") 'revy-manus-slide)
-            (define-key revy-manus-mode-map (kbd "H-<f6>") 'revy-manus-pause)
-            (define-key revy-manus-mode-map (kbd "S-<f6>") 'revy-manus-break)
+            (define-key revy-manus-mode-map (kbd "<f2>") 'revy-manus-slide)
+            (define-key revy-manus-mode-map (kbd "H-<f2>") 'revy-manus-pause)
+            (define-key revy-manus-mode-map (kbd "S-<f2>") 'revy-manus-break)
             revy-manus-mode-map)
   )
 
