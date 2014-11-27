341d690fe8c7453a426e15f7df86fc5368320c5e
Pilen <fiskomaten@gmail.com>
2014-11-27 16:59:14 +0100

    added todo for revy-build

--- emacs/uberprepare.el
+++ emacs/uberprepare.el
@@ -10,6 +10,7 @@
  We shouldn't sync till these are finished.")
 
 ;; Todo only upload when all compiles succeeded
+;; Todo deactivate all ubertex modes/ so they are rerun, aka so we renumber slides.
 (defun revy-build ()
   "Build all .tex files"
   (interactive)
