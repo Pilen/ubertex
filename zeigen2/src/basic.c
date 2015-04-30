d5e53e05fadbfc1c361f2916b79c0b0fef570652
Pilen <fiskomaten@gmail.com>
2015-04-30 04:59:09 +0200

    updated equal and added eq

--- zeigen2/src/basic.c
+++ zeigen2/src/basic.c
@@ -3,6 +3,7 @@
 #include "basic.h"
 #include "list.h"
 #include "string.h"
+#include "assert.h"
 
 Bool equal(Value a, Value b) {
     /* TODO: find out how to handle comparison of 2.0 and 2 */
@@ -22,19 +23,24 @@ Bool equal(Value a, Value b) {
         return a.val.float_val == b.val.float_val;
     case STRING:
         /* Assume the strings are normalized */
-        if (a.val.string_val -> length != b.val.string_val -> length) {
+        if (a.val.string_val -> size != b.val.string_val -> size) {
             return false;
         }
         /* eq */
-        if (a.val.string_val -> text == b.val.string_val -> text) {
+        if (a.val.string_val == b.val.string_val) {
             return true;
         }
-        Unt length = a.val.string_val -> length;
-        for (Unt i = 0; i < length; i++) {
+        Unt size = a.val.string_val -> size;
+        for (Unt i = 0; i < size; i++) {
             if (a.val.string_val -> text[i] !=
                 b.val.string_val -> text[i]) {
                 return false;
             }
+            /* NOTE: if the string can end (with '\0') before size we could end sooner */
+            /* NOTE: this could be avoided if we itterated over the length, not the size */
+            /* if (a.val.string_val -> text[i] == '\0') { */
+            /*     return true; */
+            /* } */
         }
         return true;
     case LIST:
@@ -61,12 +67,39 @@ Bool equal(Value a, Value b) {
         /* TODO: implement */
         debug("HASH VALUES CANT BE COMPARED YET!!!!");
     default:
+        assert(false)
         return false;
     }
     return false;
 }
 
-/* Bool eq(Value a, Value b); */
+Bool eq(Value a, Value b) {
+    if (a.type != b.type) {
+        return false;
+    }
+    switch (a.type) {
+    case ERROR:
+        return true;
+    case NIL:
+        return true;
+    case SYMBOL:
+        return a.val.symbol_val == b.val.symbol_val;
+    case INTEGER:
+        return a.val.integer_val == b.val.integer_val;
+    case FLOAT:
+        return a.val.float_val == b.val.float_val;
+    case STRING:
+        return a.val.string_val == b.val.string_val;
+    case LIST:
+        return a.val.string_val == b.val.string_val;
+    case HASH:
+        return a.val.hash_val == b.val.hash_val;
+    default:
+        assert(false);
+        return false;
+    }
+    return false;
+}
 
 Value copy_deep(Value value) {
     switch (value.type) {
