147462e2f2c9979f7a7f6efef0741320b5dae832
Pilen <fiskomaten@gmail.com>
2015-05-10 00:17:48 +0200

    added a communication queue lock

--- zeigen2/src/communication.h
+++ zeigen2/src/communication.h
@@ -1,6 +1,10 @@
 #ifndef Z_COMMUNICATION_H
 #define Z_COMMUNICATION_H
 
+#include <SDL2/SDL.h>
+#include "list.h"
+
 List *communication_queue;
+SDL_mutex *communication_queue_lock;
 void communication_initialize(char *host);
 #endif
