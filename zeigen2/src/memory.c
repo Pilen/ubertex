913bf3578c1691c33cfb1ba497999dc0108a8f8a
Pilen <fiskomaten@gmail.com>
2015-05-19 19:26:31 +0200

    improved memory error messages

--- zeigen2/src/memory.c
+++ zeigen2/src/memory.c
@@ -5,10 +5,11 @@
 #include "log.h"
 #include "debug.h"
 
+
 void *memory_malloc_actual(size_t size) {
     void *allocated = malloc(size);
     if (!allocated) {
-        log_fatal("Internal failure in %s\nUnable to allocate additional memory!", __func__);
+        log_fatal("Internal failure in %s\nUnable to allocate additional memory! %zd bytes requested", __func__, size);
     }
     return allocated;
 }
@@ -16,7 +17,7 @@ void *memory_malloc_actual(size_t size) {
 void *memory_malloc_cleared_actual(size_t size) {
     void *allocated = calloc(1, size);
     if (!allocated) {
-        log_fatal("Internal failure in %s\nUnable to allocate additional memory!", __func__);
+        log_fatal("Internal failure in %s\nUnable to allocate additional memory! %zd bytes requested", __func__, size);
     }
     return allocated;
 
@@ -25,7 +26,7 @@ void *memory_malloc_cleared_actual(size_t size) {
 void *memory_calloc_actual(size_t amount, size_t size) {
     void *allocated = calloc(amount, size);
     if (!allocated) {
-        log_fatal("Internal failure in %s\nUnable to allocate additional memory!", __func__);
+        log_fatal("Internal failure in %s\nUnable to allocate additional memory! %zd bytes requested", __func__, amount * size);
     }
     return allocated;
 
