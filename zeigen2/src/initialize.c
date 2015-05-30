0a22dff32e1c773d77d02edd6f0411ba9204b958
Pilen <fiskomaten@gmail.com>
2015-05-30 13:38:42 +0200

    debugging

--- zeigen2/src/initialize.c
+++ zeigen2/src/initialize.c
@@ -150,15 +150,14 @@ void initialize_SDL(Environment *environment, Bool fullscreen) {
     int h = 768;
     flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
 
-    /* if (!fullscreen) { */
-    /*     x = 0; */
-    /*     y = 0; */
-    /*     w = 400; */
-    /*     h = 400; */
-    /*     flags = 0; */
-    /* } */
+    if (!fullscreen) {
+        x = 0;
+        y = 0;
+        w = 400;
+        h = 400;
+        flags = 0;
+    }
 
-    flags = SDL_WINDOW_FULLSCREEN;
     debugi(flags);
     SDL_Window *window = SDL_CreateWindow(OPTION_PROGRAM_NAME, x, y, w, h, flags);
     if (!window) {
