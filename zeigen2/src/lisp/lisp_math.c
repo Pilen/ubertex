8ed7a3fa272a6e8bd950aa288620ee1ae1ce857c
Pilen <fiskomaten@gmail.com>
2015-06-18 01:56:40 +0200

    added some handy math functions for lisp

--- zeigen2/src/lisp/lisp_math.c
+++ zeigen2/src/lisp/lisp_math.c
@@ -1,11 +1,15 @@
+
+#include "../math.h"
 #include "../debug.h"
 #include "../types.h"
 #include "../eval.h"
 #include "../list.h"
+#include "../symbol.h"
+
 
 LISP_BUILTIN(plus, "") {
     Int int_sum = 0;
-    Double float_sum = 0;
+    Double float_sum = 0.0;
 
     Bool real = false;
     for (Unt i = 1; i < args -> length; i++) {
@@ -35,7 +39,7 @@ LISP_BUILTIN(minus, "") {
         return VALUE_INTEGER(0);
     }
     Int int_sum = 0;
-    Double float_sum = 0;
+    Double float_sum = 0.0;
 
     Bool real = false;
 
@@ -81,4 +85,108 @@ LISP_BUILTIN(minus, "") {
     } else {
         return VALUE_INTEGER(int_sum);
     }
+}
+
+LISP_BUILTIN(times, "") {
+    Int int_product = 1;
+    Double float_product = 1.0;
+
+    Bool real = false;
+    for (Unt i = 1; i < args -> length; i++) {
+        Value arg = LIST_GET_UNSAFE(args, i);
+        switch (arg.type) {
+        case INTEGER:
+            int_product *= arg.val.integer_val;
+            break;
+        case FLOAT:
+            float_product *= arg.val.float_val;
+            real = true;
+            break;
+        default:
+            /* TODO: log error */
+            return VALUE_ERROR;
+        }
+    }
+    if (real) {
+        return VALUE_FLOAT(float_product + int_product);
+    } else {
+        return VALUE_INTEGER(int_product);
+    }
+}
+
+LISP_BUILTIN(greater_than, "") {
+    if (args -> length < 1) {
+        return VALUE_ERROR;
+    }
+
+    Value previous = LIST_GET_UNSAFE(args, 1);
+
+    for (Unt i = 2; i < args -> length; i++) {
+        Value current = LIST_GET_UNSAFE(args, i);
+        switch (previous.type) {
+        case INTEGER:
+            switch (current.type) {
+            case INTEGER:
+                if (previous.val.integer_val > current.val.integer_val) {
+                    previous = current;
+                    continue;
+                } else {
+                    return VALUE_NIL;
+                }
+            case FLOAT:
+                if (previous.val.integer_val > current.val.float_val) {
+                    previous = current;
+                    continue;
+                } else {
+                    return VALUE_NIL;
+                }
+            default:
+                return VALUE_ERROR;
+            }
+        case FLOAT:
+            switch (current.type) {
+            case INTEGER:
+                if (previous.val.float_val > current.val.integer_val) {
+                    previous = current;
+                    continue;
+                } else {
+                    return VALUE_NIL;
+                }
+            case FLOAT:
+                if (previous.val.float_val > current.val.float_val) {
+                    previous = current;
+                    continue;
+                } else {
+                    return VALUE_NIL;
+                }
+            default:
+                return VALUE_ERROR;
+            }
+        default:
+            return VALUE_ERROR;
+        }
+    }
+    return symbols_t;
+}
+
+
+LISP_BUILTIN(sin, "") {
+    if (args -> length != 2) {
+        return VALUE_ERROR;
+    }
+
+    Value angle_v = LIST_GET_UNSAFE(args, 1);
+    Double angle;
+    switch (angle_v.type) {
+    case INTEGER:
+        angle = angle_v.val.integer_val;
+        break;
+    case FLOAT:
+        angle = angle_v.val.float_val;
+        break;
+    default:
+        return VALUE_ERROR;
+    }
+    Double result = RAD_TO_DEG(sin(DEG_TO_RAD(angle)));
+    return VALUE_FLOAT(result);
 }
--- zeigen2/src/math.h
+++ zeigen2/src/math.h
@@ -2,6 +2,13 @@
 #define Z_MATH_H
 
 #include "types.h"
+#include <math.h>
+
+#define RAD_TO_DEG(radians) ((radians) * 180.0 / M_PI)
+#define DEG_TO_RAD(degree) ((degree) * M_PI / 180.0)
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 Unt round_up_to_power_of_2(Unt n);
 #define MIN(x, y) (((x) <= (y)) ? (x) : (y))
