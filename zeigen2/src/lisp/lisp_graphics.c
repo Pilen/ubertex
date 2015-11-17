136b9e8db3e632ed550fea4e0b25dbcdc3569d2f
Pilen <fiskomaten@gmail.com>
2015-11-17 22:26:00 +0100

    debugging text

--- zeigen2/src/lisp/lisp_graphics.c
+++ zeigen2/src/lisp/lisp_graphics.c
@@ -201,6 +201,7 @@ LISP_BUILTIN(text, "") {
     image.y = 0;
     debug("rendering text:")
     SDL_RenderCopy(environment -> renderer, texture, NULL, &image);
+    SDL_RenderDrawLine(environment -> renderer, 0, 0, 100, 100);
     /* graphics_render_at_position(environment, texture, position); */
     return VALUE_NIL;
 }
